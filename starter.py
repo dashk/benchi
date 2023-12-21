2972da1ed106675671580726cd9f7bb044aab16f
dashk <dashk@users.noreply.github.com>
2023-12-21 08:47:39 +0800

    Question generation

--- starter.py
+++ starter.py
@@ -8,20 +8,20 @@ from llama_index import (
     StorageContext,
     load_index_from_storage,
 )
+from llama_index.evaluation import DatasetGenerator
 from llama_index.llms import Ollama
 
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 INDEX_STORAGE_PATH = "./storage"
 llm = Ollama(model="llama2")
 service_context = ServiceContext.from_defaults(llm=llm)
+documents = SimpleDirectoryReader("data").load_data()
 
 # check if storage already exists
 if not os.path.exists(INDEX_STORAGE_PATH):
-    # load the documents and create the index
-    documents = SimpleDirectoryReader("data").load_data()
     index = VectorStoreIndex.from_documents(
         documents,
         service_context=service_context,
@@ -33,7 +33,12 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=INDEX_STORAGE_PATH)
     index = load_index_from_storage(storage_context)
 
+data_generator = DatasetGenerator.from_documents(documents)
+eval_questions = data_generator.generate_questions_from_nodes()
+
+print(eval_questions)
+
 # either way we can now query the index
-query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
-print(response)
+# query_engine = index.as_query_engine()
+# response = query_engine.query("What did the author do growing up?")
+# print(response)

f7e1b8be0e4194599659dedc5d4ca95456443d2d
dashk <dashk@users.noreply.github.com>
2023-12-22 11:30:08 +0800

    Use mistral for everything Explored data generation

--- starter.py
+++ starter.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import logging
 import sys
 import os.path
@@ -8,15 +9,17 @@ from llama_index import (
     StorageContext,
     load_index_from_storage,
 )
-from llama_index.evaluation import DatasetGenerator
+from llama_index.evaluation import DatasetGenerator, RelevancyEvaluator
 from llama_index.llms import Ollama
 
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 INDEX_STORAGE_PATH = "./storage"
-llm = Ollama(model="llama2")
+LIMIT = 1
+
+llm = Ollama(model="mistral:7b")
 service_context = ServiceContext.from_defaults(llm=llm)
 documents = SimpleDirectoryReader("data").load_data()
 
@@ -33,10 +36,56 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=INDEX_STORAGE_PATH)
     index = load_index_from_storage(storage_context)
 
-data_generator = DatasetGenerator.from_documents(documents)
-eval_questions = data_generator.generate_questions_from_nodes()
+data_generator = DatasetGenerator.from_documents(documents, service_context=service_context)
+eval_questions = data_generator.generate_questions_from_nodes(num=1)
 
 print(eval_questions)
+
+evaluator = RelevancyEvaluator(service_context=service_context)
+
+# create vector index
+vector_index = VectorStoreIndex.from_documents(
+    documents, service_context=service_context
+)
+query_engine = vector_index.as_query_engine()
+
+questions_and_results = []
+
+@dataclass
+class Question:
+    question: str
+    answer: str
+    reasoning: str
+    score: float
+
+
+for i, question in enumerate(eval_questions):
+    if i >= LIMIT:
+        break
+    logging.info("Processing %i/%i", i, len(eval_questions))
+
+    logging.info("Query: %s", question)
+    response_vector = query_engine.query(question)
+    logging.info("Vector response: %s", response_vector)
+
+    logging.info("Evaluation")
+    eval_result = evaluator.evaluate_response(
+        query=question, response=response_vector
+    )
+    logging.info("Result: %s", eval_result)
+    questions_and_results.append(
+        Question(
+            question=eval_result.query,
+            answer=eval_result.response,
+            reasoning=eval_result.feedback,
+            score=eval_result.score,
+        )
+    )
+
+# Output evaluation results to a JSONL file
+with open("evaluation_results.jsonl", "w") as f:
+    for q in questions_and_results:
+        f.write(f"{q}\n")
 
 # either way we can now query the index
 # query_engine = index.as_query_engine()
